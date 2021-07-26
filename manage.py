1f9a969ea7486e3de9aba6f9750ac81f7c29b007
Kareem Zidane <kzidane@cs50.harvard.edu>
2021-07-26 08:35:22 -0400

    Remove MigrateCommand

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
+from flask_migrate import Migrate
 from sqlalchemy.sql import func
 import flask_migrate
 import os
@@ -15,7 +15,6 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 manager = Manager(app)
-manager.add_command('db', MigrateCommand)
 
 class Output(db.Model):
     __tablename__ = "outputs"
